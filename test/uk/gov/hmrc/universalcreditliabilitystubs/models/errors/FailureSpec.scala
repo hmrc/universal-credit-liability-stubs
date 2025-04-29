4d5d203fdc886079af206bb6465e94fb9f7e8c52
Paul Olabisi <18728756+paulonevrything@users.noreply.github.com>
2025-04-29 12:08:25 +0100

    Added unit tests for model classes

---
+++ test/uk/gov/hmrc/universalcreditliabilitystubs/models/errors/FailureSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.universalcreditliabilitystubs.models.errors
+
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.Json
+
+class FailureSpec extends AnyWordSpec with Matchers {
+
+  "must write to correct json" in {
+
+    val model = Failure(
+      reason = "Forbidden",
+      code = "403.2"
+    )
+
+    val expectedJson = Json.obj(
+      "reason" -> "Forbidden",
+      "code"   -> "403.2"
+    )
+
+    Json.toJson(model) mustBe expectedJson
+  }
+
+}
