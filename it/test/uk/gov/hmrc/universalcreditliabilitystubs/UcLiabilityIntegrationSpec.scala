bb4ca40e0a7968b5d83dc3b34f0309de7d608565
dananjaiahnemmadi <202163272+dananjaiahnemmadi@users.noreply.github.com>
2025-05-02 16:08:52 +0100

    Created Insert and Terminate automation regression cases for 204

--- it/test/uk/gov/hmrc/universalcreditliabilitystubs/UcLiabilityIntegrationSpec.scala
+++ it/test/uk/gov/hmrc/universalcreditliabilitystubs/UcLiabilityIntegrationSpec.scala
@@ -23,6 +23,7 @@ import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.ws.WSClient
+import play.api.libs.ws.DefaultBodyWritables.*
 
 class UcLiabilityIntegrationSpec
     extends PlaySpec
@@ -44,6 +45,202 @@ class UcLiabilityIntegrationSpec
           .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
           .execute("POST")
           .futureValue
+
+      response.status mustBe 204
+    }
+  }
+
+  "UC Liability Insert LCW/LCWRA no end date" must {
+    "respond with 204 status" in {
+
+      val payload =
+        s"""{
+            "universalCreditRecordType": "LCW/LCWRA",
+            "universalCreditAction": "Insert",
+            "dateOfBirth": "2002-10-10",
+            "liabilityStartDate": "2025-08-19"
+      }""".stripMargin
+
+      val response = wsClient
+          .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
+          .withHttpHeaders(("Content-Type", "application/json"))
+          .post(payload)
+          .futureValue
+          
+      response.status mustBe 204
+    }
+  }
+
+  "UC Liability Insert LCW/LCWRA with end date" must {
+    "respond with 204 status" in {
+
+      val payload =
+        s"""{
+            "universalCreditRecordType": "LCW/LCWRA",
+            "universalCreditAction": "Insert",
+            "dateOfBirth": "2002-10-10",
+            "liabilityStartDate": "2025-08-19",
+            "liabilityEndDate": "2025-01-04"
+        }""".stripMargin
+
+      val response = wsClient
+        .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
+        .withHttpHeaders(("Content-Type", "application/json"))
+        .post(payload)
+        .futureValue
+
+      response.status mustBe 204
+    }
+  }
+
+  "UC Liability - Insert UC (S2P) no end date" must {
+    "respond with 204 status" in {
+
+      val payload =
+        s"""{
+             "universalCreditRecordType": "UC (S2P)",
+             "universalCreditAction": "Insert",
+             "dateOfBirth": "2002-10-10",
+             "liabilityStartDate": "2025-08-19"
+       }""".stripMargin
+
+      val response = wsClient
+        .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
+        .withHttpHeaders(("Content-Type", "application/json"))
+        .post(payload)
+        .futureValue
+
+      response.status mustBe 204
+    }
+  }
+
+  "UC Liability - Insert UC (S2P) with end date" must {
+    "respond with 204 status" in {
+
+      val payload =
+        s"""{
+            "universalCreditRecordType": "UC (S2P)",
+            "universalCreditAction": "Insert",
+            "dateOfBirth": "2002-10-10",
+            "liabilityStartDate": "2025-08-19",
+            "liabilityEndDate": "2025-01-04"
+         }""".stripMargin
+
+      val response = wsClient
+        .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
+        .withHttpHeaders(("Content-Type", "application/json"))
+        .post(payload)
+        .futureValue
+
+      response.status mustBe 204
+    }
+  }
+
+  "UC Liability - Insert UC no end date" must {
+    "respond with 204 status" in {
+
+      val payload =
+        s"""{
+             "universalCreditRecordType": "UC",
+             "universalCreditAction": "Insert",
+             "dateOfBirth": "2002-10-10",
+             "liabilityStartDate": "2025-08-19"
+       }""".stripMargin
+
+      val response = wsClient
+        .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
+        .withHttpHeaders(("Content-Type", "application/json"))
+        .post(payload)
+        .futureValue
+
+      response.status mustBe 204
+    }
+  }
+
+  "UC Liability - Insert UC with end date" must {
+    "respond with 204 status" in {
+
+      val payload =
+        s"""{
+            "universalCreditRecordType": "UC",
+            "universalCreditAction": "Insert",
+            "dateOfBirth": "2002-10-10",
+            "liabilityStartDate": "2025-08-19",
+            "liabilityEndDate": "2025-01-04"
+         }""".stripMargin
+
+      val response = wsClient
+        .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
+        .withHttpHeaders(("Content-Type", "application/json"))
+        .post(payload)
+        .futureValue
+
+      response.status mustBe 204
+    }
+  }
+
+
+  "UC Liability Terminate LCW/LCWRA with end date" must {
+    "respond with 204 status" in {
+
+      val payload =
+        s"""{
+            "universalCreditRecordType": "LCW/LCWRA",
+            "universalCreditAction": "Terminate",
+            "dateOfBirth": "2002-10-10",
+            "liabilityStartDate": "2025-08-19",
+            "liabilityEndDate": "2025-01-04"
+          }""".stripMargin
+
+      val response = wsClient
+        .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
+        .withHttpHeaders(("Content-Type", "application/json"))
+        .post(payload)
+        .futureValue
+
+      response.status mustBe 204
+    }
+  }
+
+  "UC Liability Terminate UC (S2P) with end date" must {
+    "respond with 204 status" in {
+
+      val payload =
+        s"""{
+            "universalCreditRecordType": "UC (S2P)",
+            "universalCreditAction": "Terminate",
+            "dateOfBirth": "2002-10-10",
+            "liabilityStartDate": "2025-08-19",
+            "liabilityEndDate": "2025-01-04"
+          }""".stripMargin
+
+      val response = wsClient
+        .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
+        .withHttpHeaders(("Content-Type", "application/json"))
+        .post(payload)
+        .futureValue
+
+      response.status mustBe 204
+    }
+  }
+
+  "UC Liability Terminate UC with end date" must {
+    "respond with 204 status" in {
+
+      val payload =
+        s"""{
+            "universalCreditRecordType": "UC",
+            "universalCreditAction": "Terminate",
+            "dateOfBirth": "2002-10-10",
+            "liabilityStartDate": "2025-08-19",
+            "liabilityEndDate": "2025-01-04"
+          }""".stripMargin
+
+      val response = wsClient
+        .url(s"$baseUrl/universal-credit-liability-stubs/person/nino/liability/universal-credit")
+        .withHttpHeaders(("Content-Type", "application/json"))
+        .post(payload)
+        .futureValue
 
       response.status mustBe 204
     }
