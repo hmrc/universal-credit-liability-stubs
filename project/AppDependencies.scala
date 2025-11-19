54f8950ae7187e4d12100317b8ba562c463923eb
Daniel McGlinchey <166512634+DMcGlinchey@users.noreply.github.com>
2025-11-19 14:29:24 +0000

    scala sbt library plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"      %% "bootstrap-backend-play-30"      % bootstrapVersion,
-    "org.typelevel"    %% "cats-core"                      % "2.13.0"
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "org.typelevel" %% "cats-core"                 % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
@@ -17,6 +17,6 @@ object AppDependencies {
   )
 
   val it: Seq[ModuleID] = Seq(
-    "com.atlassian.oai" % "swagger-request-validator-core" % "2.44.9"
+    "com.atlassian.oai" % "swagger-request-validator-core" % "2.46.0"
   )
 }
