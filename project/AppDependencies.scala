7c1f97f536c84187e4ffcb10ea9fb8264297c19b
Nikolas <gianick@users.noreply.github.com>
2025-12-11 20:33:04 +0000

    Minor refactoring to AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,16 +5,16 @@ object AppDependencies {
   private val bootstrapVersion = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"      %% "bootstrap-backend-play-30"      % bootstrapVersion,
-    "org.typelevel"    %% "cats-core"                      % "2.13.0"
+    "org.typelevel" %% "cats-core"                 % "2.13.0",
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.scalatest"        %% "scalatest"              % "3.2.19"         % Test,
-    "org.scalatestplus"    %% "scalacheck-1-18"        % "3.2.19.0"       % Test,
-    "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"
-  )
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0",
+    "org.scalatest"        %% "scalatest"              % "3.2.19",
+    "org.scalatestplus"    %% "scalacheck-1-18"        % "3.2.19.0",
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30" % bootstrapVersion
+  ).map(_ % Test)
 
   val it: Seq[ModuleID] = Seq(
     "com.atlassian.oai" % "swagger-request-validator-core" % "2.46.0"
