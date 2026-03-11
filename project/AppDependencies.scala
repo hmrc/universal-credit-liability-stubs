ef55eb24bdecfdecf76c7a8fc88d30e116d71c08
Nikolas <gianick@users.noreply.github.com>
2026-03-11 15:45:58 +0000

    Bumped bootstrap version to 10.7.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "org.typelevel" %% "cats-core"                 % "2.13.0",
